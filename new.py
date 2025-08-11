5ed77865ff2e3b2ecf4bed56d10a2d43a2028122
Arjun Mishra <arjunmishra2903@gmail.com>
2025-08-11 16:31:07 +0530

    Update new.py

--- new.py
+++ new.py
@@ -4,3 +4,7 @@
 ### Pankha manch par gir gya , Jaan jaa rhi .
 ### Maine bola papa se , bat dilaado mere liye, 
 ### Darling badnaaam hui , tere liye.
+
+
+##  PEECHE TAASH KHELI JAA RHI ,
+##  GYAN KI BAATEIN PELI JAA RHI
