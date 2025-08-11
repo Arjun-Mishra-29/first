1e48c57f4041bf41f7749496d9f9a35f1106e3bf
Ankit Kumar Yadav <140153740+AY54189@users.noreply.github.com>
2025-08-11 16:27:58 +0530

    Update new.py

--- new.py
+++ new.py
@@ -1,1 +1,6 @@
 ## i am arjun jhatu
+
+### KharKhara Pair par chadh gya , jaan jaa rhi. 
+### Pankha manch par gir gya , Jaan jaa rhi .
+### Maine bola papa se , bat dilaado mere liye, 
+### Darling badnaaam hui , tere liye.
